d57be9b484ba3072a27fa89765e1dc14a5fb156a
Diogo Righi <diogorighi@gmail.com>
2016-05-13 19:36:11 -0300

    add post and put methods to routes

--- src/api/index.js
+++ src/api/index.js
@@ -17,7 +17,30 @@ router.get('/todos', function(req,res){
 
 // TODO: Add POST route to create new entries
 
+router.post('/todos', function(req, res){
+	var todo = req.body;
+	Todo.create(todo, function(err, todo){
+		if(err){
+			return res.status(500).json({ error: err.message })
+		}
+		res.json({todo: todo, message: "Todo created!"});
+	});
+})
+
 // TODO: Add PUT route to update existing entries
+router.put('/todos/:id', function(req, res){
+	var id = req.params.id;
+	var todo = req.body;
+	if(todo && todo._id !== id){
+		return res.status(500).json({ error: err.message })
+	}
+	Todo.findOneAndUpdate(id, todo, {new: true}, function(err, todo){
+		if(err){
+			return res.status(500).json({ error: err.message })
+		}
+		res.json({todo: todo, message: "Todo updated!"});
+	});
+})
 
 // TODO: Add DELETE route to delete entries
 
--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 'use strict';
 
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = require('./api');
+
 var app = express();
 
 require('./database');
 require('./seed')
 
 app.use('/', express.static('public'));
+app.use(bodyParser.json());
+
 app.use('/api', router);
 
 app.listen(3000, function(){
